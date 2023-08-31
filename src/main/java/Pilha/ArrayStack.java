2f599ba4b971f9e611396493e8642b8f09cea3b4
antonio.trajano <ansetrcaastc@gmai.com>
2023-08-31 16:02:07 -0300

    adicionado classe stack e alguns métodos dessa classe

---
+++ src/main/java/Pilha/ArrayStack.java
@@ -0,0 +1,28 @@
+package Pilha;
+
+public class ArrayStack<E> implements Stack<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E top() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E pop() throws EmptyStackException {
+        return null;
+    }
+}
