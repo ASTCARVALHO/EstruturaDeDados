86025678dc4e62db5ed0ebea7d28c48d2e3a1f11
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-09-02 15:00:28 -0300

    adicionado a exceção EmptyQueueException

---
+++ src/main/java/Lista/EmptyQueueException.java
@@ -0,0 +1,10 @@
+package Lista;
+
+public class EmptyQueueException extends RuntimeException{
+    public EmptyQueueException(String msg){
+        super(msg);
+    }
+    public EmptyQueueException(){
+        super();
+    }
+}
