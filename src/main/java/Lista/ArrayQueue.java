1caa23c0261512958e7192a9b5587dcc1fb6936a
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-09-03 14:14:37 -0300

    algumas mudanças no classe lista

--- src/main/java/Lista/ArrayQueue.java
+++ src/main/java/Lista/ArrayQueue.java
@@ -3,28 +3,19 @@ package Lista;
 public class ArrayQueue <E> implements Queue<E> {
     private int head = 0;
     private int tail = 0;
-    private int count = 0;
     private int capacity;
-    private static final int INITCAPACITY = 10;
     private E[] array;
     public ArrayQueue(int cap){
         capacity = cap;
         array = (E[]) new Object[capacity];
     }
-    public ArrayQueue(){
-        this(INITCAPACITY);
-    }
-
     public int size() {
-        return array.length;
+        return (capacity - head + tail) % capacity;
     }
-
 
     public boolean isEmpty() {
-        return count == 0;
+        return head == tail;
     }
-
-    @Override
     public boolean isFull() {
         return false;
     }
@@ -36,29 +27,28 @@ public class ArrayQueue <E> implements Queue<E> {
 
 
     public void enqueue(E e) {
-        count ++;
         array[tail] = e;
        tail = (tail+1) % size();
-       if(count == array.length) resize(capacity * 2);
+       if(size() == array.length) resize(capacity * 2);
 
     }
 
-
     public E dequeue() throws EmptyQueueException {
-        count --;
         E e = array[head];
         head = (head +1) % size();
-        if(count > 0 && count == array.length/4) resize(capacity /2);
+        if(size() > 0 && size() == array.length/4) resize(capacity /2);
 
         return e;
     }
     public void resize(int newSize){
+        capacity = newSize;
+        int size = size();
         E[] temp = (E[]) new Object[newSize];
-        for (int i = 0; i < count -1; i++) {
+        for (int i = 0; i < size() ; i++) {
             temp[i] = array[(i + head) % capacity];
         }
         array  = temp;
         head = 0;
-        tail = 0;
+        tail = size;
     }
 }
