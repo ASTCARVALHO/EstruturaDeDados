6abcc308053f36e13a459c5970ad35bd0d1aa453
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-09-03 03:23:30 -0300

    iniciando os testes da fila

--- src/main/java/Lista/ArrayQueue.java
+++ src/main/java/Lista/ArrayQueue.java
@@ -24,6 +24,11 @@ public class ArrayQueue <E> implements Queue<E> {
         return count == 0;
     }
 
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
 
     public E front() throws EmptyQueueException {
         return array[head];
@@ -31,17 +36,29 @@ public class ArrayQueue <E> implements Queue<E> {
 
 
     public void enqueue(E e) {
-        array[tail] = e;
         count ++;
+        array[tail] = e;
        tail = (tail+1) % size();
+       if(count == array.length) resize(capacity * 2);
 
     }
 
 
     public E dequeue() throws EmptyQueueException {
+        count --;
         E e = array[head];
         head = (head +1) % size();
+        if(count > 0 && count == array.length/4) resize(capacity /2);
 
         return e;
+    }
+    public void resize(int newSize){
+        E[] temp = (E[]) new Object[newSize];
+        for (int i = 0; i < count -1; i++) {
+            temp[i] = array[(i + head) % capacity];
+        }
+        array  = temp;
+        head = 0;
+        tail = 0;
     }
 }
--- src/main/java/Lista/Queue.java
+++ src/main/java/Lista/Queue.java
@@ -3,6 +3,7 @@ package Lista;
 public interface Queue<E>  {
     public int size();
     public boolean isEmpty();
+    public boolean isFull();
     public E front() throws EmptyQueueException;
     public void enqueue(E e);
     public E dequeue() throws EmptyQueueException;
