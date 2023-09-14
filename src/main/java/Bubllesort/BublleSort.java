906aa11f10b6c41a462f8f538f6ab504620ed031
antonio.trajano <ansetrcaastc@gmail.com>
2023-09-14 14:12:37 -0300

    iserindo comentários para melhor entendimento

---
+++ src/main/java/Bubllesort/BublleSort.java
@@ -0,0 +1,19 @@
+package Bubllesort;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BublleSort {
+    public static void main(String[] args) {
+        int[] numeros;
+        numeros = new List<int>[];
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length; j++) {
+                if(numeros[j] > numeros[j+1]){
+
+                }
+            }
+        }
+    }
+}
