48edacc2c50c54e8ac7ccddee2ad58f29401a275
antonio.trajano <ansetrcaastc@gmail.com>
2023-09-26 10:23:36 -0300

    implementando SelectionSort

--- src/main/java/AlgoritmosOrdenação/SelectionSort.java
+++ src/main/java/AlgoritmosOrdenação/SelectionSort.java
@@ -3,5 +3,18 @@ package AlgoritmosOrdenação;
 import java.util.ArrayList;
 
 public class SelectionSort {
+    public static void selectionSort(int[] lista){
+        for (int i = 0; i < lista.length; i++) {
+            int min = i;
+            for (int j = 0; j < lista.length; j++) {
+               if(lista[j] < lista[min]) min = j;
+            }
+            if (lista[i] != lista[min]){
+               int aux = lista[i];
+               lista[i] = lista[min];
+               lista[min] =  aux;
+            }
+        }
+    }
 
 }
