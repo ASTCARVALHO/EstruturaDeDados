a7930ebea43036aada36fe960af446766900f631
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-10-15 16:26:19 -0300

    Test bubbleSort

---
+++ src/test/java/AlgoritmosOrdenação/BubbleSortTest.java
@@ -0,0 +1,28 @@
+package AlgoritmosOrdenação;
+
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Random;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+    int[] numeros;
+
+    BubbleSortTest() {
+        numeros = new int[10];
+    }
+    @Test
+    void testaOrdenacao(){
+        Random random = new Random();
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = random.nextInt(100);
+        }
+        System.out.println("Desordenado: ");
+        System.out.println(Arrays.toString(numeros));
+
+        BubbleSort.bubbleSort(numeros);
+
+        System.out.println("Ordenado");
+        System.out.println(Arrays.toString(numeros));
+    }
+}
